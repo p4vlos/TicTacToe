e129bf3457ac0642bf2108ff0354c8e7c4ea1126
Pavlos Nicolaou <nicolaou_paul@hotmail.com>
2016-11-07 11:34:14 +0000

    added constraints and started coding the game

--- Tic Tac Toe game/ViewController.swift
+++ Tic Tac Toe game/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+   
+    var gameStart = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var combinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    var playerTurn = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set player 1
+        playerTurn = 1
+        //update turn label
+        turnLabel.text = "It is Player's \(playerTurn) turn!"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetButton(_ sender: Any) {
+        
+        //clear button
+        btn1.setTitle("", for: <#T##UIControlState#>)
+        btn2.setTitle("", for: <#T##UIControlState#>)
+        btn3.setTitle("", for: <#T##UIControlState#>)
+        btn4.setTitle("", for: <#T##UIControlState#>)
+        btn5.setTitle("", for: <#T##UIControlState#>)
+        btn6.setTitle("", for: <#T##UIControlState#>)
+        btn7.setTitle("", for: <#T##UIControlState#>)
+        btn8.setTitle("", for: <#T##UIControlState#>)
+        btn9.setTitle("", for: <#T##UIControlState#>)
+
+        //update turn label
+        playerTurn = 1
+        turnLabel.text = "It is Player's \(playerTurn) turn!"
+        
+        
+    }
 
 }
 
